c47c0c1be549bc424e07d0588cd456bbcd1f7eb5
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-11-10 07:08:50 +0300

    defined routes

---
+++ src/components/user/DonationsForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DonationsForm = () => {
+  return (
+    <div>DonationsForm</div>
+  )
+}
+
+export default DonationsForm
---
+++ src/components/user/VisitForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VisitForm = () => {
+  return (
+    <div>VisitForm</div>
+  )
+}
+
+export default VisitForm
